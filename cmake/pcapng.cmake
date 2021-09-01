e1e56f45ed2ffd0e5e57c0a31fe5561313148673
houssem <houssem.benali@technica-engineering.de>
2021-09-01 12:24:20 +0100

    Initial commit

---
+++ cmake/pcapng.cmake
@@ -0,0 +1,11 @@
+cmake_minimum_required(VERSION 3.11)
+
+include(FetchContent)
+
+FetchContent_Declare(
+  pcapng
+  GIT_REPOSITORY    https://github.com/Technica-Engineering/LightPcapNg.git
+  GIT_TAG           3c848a6
+)
+
+FetchContent_MakeAvailable(pcapng)
